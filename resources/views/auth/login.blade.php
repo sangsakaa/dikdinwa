81ac958420e9b72b9edc35e1c057ba01a24f9548
sangsakaa <zainuri717@gmail.com>
2023-01-23 04:01:18 +0700

    build hero

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,7 +15,7 @@
 
                     <x-input-with-icon-wrapper>
                         <x-slot name="icon">
-                            <x-icons.github aria-hidden="true" class="w-5 h-5" />
+                            <x-heroicon-o-moon aria-hidden="true" class="w-5 h-5" />
                         </x-slot>
                         <x-input withicon id="email" class="block w-full" type="email" name="email" :value="old('email')" placeholder="{{ __('Email') }}" required autofocus />
                     </x-input-with-icon-wrapper>
@@ -27,7 +27,7 @@
 
                     <x-input-with-icon-wrapper>
                         <x-slot name="icon">
-                            <x-icons.github aria-hidden="true" class="w-5 h-5" />
+                            <x-heroicon-o-moon aria-hidden="true" class="w-5 h-5" />
                         </x-slot>
                         <x-input withicon id="password" class="block w-full" type="password" name="password" required autocomplete="current-password" placeholder="{{ __('Password') }}" />
                     </x-input-with-icon-wrapper>
@@ -49,7 +49,7 @@
 
                 <div>
                     <x-button class="justify-center w-full gap-2">
-                        <x-icons.github class="w-6 h-6" aria-hidden="true" />
+                        <x-heroicon-o-moon class="w-6 h-6" aria-hidden="true" />
                         <span>{{ __('Log in') }}</span>
                     </x-button>
                 </div>
