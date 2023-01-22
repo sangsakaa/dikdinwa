b992d44686e65e853879872ce915caaea7f60d9c
sangsakaa <zainuri717@gmail.com>
2023-01-23 04:15:36 +0700

    veri

--- resources/views/components/sidebar/content.blade.php
+++ resources/views/components/sidebar/content.blade.php
@@ -7,7 +7,7 @@
     </x-sidebar.link>
     <x-sidebar.dropdown title="Buttons" :active="Str::startsWith(request()->route()->uri(), 'buttons')">
         <x-slot name="icon">
-            <x-heroicon-o-view-grid class="flex-shrink-0 w-6 h-6" aria-hidden="true" />
+            <x-icons.github class="flex-shrink-0 w-6 h-6" aria-hidden="true" />
         </x-slot>
         <x-sidebar.sublink title="Text button" href="{{ route('buttons.text') }}" :active="request()->routeIs('buttons.text')" />
         <x-sidebar.sublink title="Icon button" href="{{ route('buttons.icon') }}" :active="request()->routeIs('buttons.icon')" />
