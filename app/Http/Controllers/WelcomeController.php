31c3215ca0f1f79a4f5f886f17a82b134ab8c3bc
sangsakaa <zainuri717@gmail.com>
2023-09-08 08:28:08 +0700

    ok

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,13 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Siswa;
+
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\DB;
 
 class WelcomeController extends Controller
 {
     public function index()
     {
-        $a = 100;
-        return view('dashboard', compact('a'));
+        $dataSiswa = Siswa::query()
+            ->whereIn('madrasah_diniyah', ['Ulya', 'Wustho', 'Ula'])
+            ->groupBy('madrasah_diniyah', 'jenis_kelamin')
+            ->select('madrasah_diniyah', 'jenis_kelamin',  DB::raw('count(*) as total'))
+            ->get();
+        $dataSiswaMadin = Siswa::query()
+            ->whereIn('madrasah_diniyah', ['Ulya', 'Wustho', 'Ula'])
+            ->groupBy('madrasah_diniyah')
+            ->select('madrasah_diniyah',  DB::raw('count(*) as total'))
+            ->get();
+
+        return view('dashboard', [
+            'dataSiswa' => $dataSiswa,
+            'dataSiswaMadin' => $dataSiswaMadin,
+        ]);
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,8 +12,90 @@
     </x-slot>
 
     <div class="p-6 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1">
-        {{ __("You're logged in!")  }}
-        {{$a}}
+        <div>
+            <div class=" bg-white  justify-between ">
+                <span>Dalam Progress Pengembangan</span>
+                <div class=" w-full grid grid-cols-1 sm:grid-cols-2 gap-2">
+                    <div>
+                        <canvas id="grafik-siswa-id"></canvas>
+                    </div>
+                    <div>
+                        <canvas id="grafik-siswa"></canvas>
+                    </div>
+                </div>
+                <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+                <script>
+                    document.addEventListener('DOMContentLoaded', function() {
+                        var data = @json($dataSiswa);
+                        var labels = [];
+                        var counts = [];
 
-    </div>
+                        data.forEach(function(item) {
+                            labels.push(item.madrasah_diniyah + ' - ' + item.jenis_kelamin);
+                            counts.push(item.total);
+                        });
+
+                        var ctx = document.getElementById('grafik-siswa').getContext('2d');
+                        var myChart = new Chart(ctx, {
+                            type: 'bar',
+                            data: {
+                                labels: labels,
+                                datasets: [{
+                                    label: 'Jumlah Siswa',
+                                    data: counts,
+                                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                                    borderColor: 'rgba(75, 192, 192, 1)',
+                                    borderWidth: 1
+                                }]
+                            },
+                            options: {
+                                scales: {
+                                    y: {
+                                        beginAtZero: true,
+                                        stepSize: 1
+                                    }
+                                }
+                            }
+                        });
+                    });
+                </script>
+                <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+                <script>
+                    document.addEventListener('DOMContentLoaded', function() {
+                        var data = @json($dataSiswaMadin);
+                        var labels = [];
+                        var counts = [];
+
+                        data.forEach(function(item) {
+                            labels.push(item.madrasah_diniyah);
+                            counts.push(item.total);
+                        });
+
+                        var ctx = document.getElementById('grafik-siswa-id').getContext('2d');
+                        var myChart = new Chart(ctx, {
+                            type: 'bar',
+                            data: {
+                                labels: labels,
+                                datasets: [{
+                                    label: 'Jumlah Siswa',
+                                    data: counts,
+                                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                                    borderColor: 'rgba(75, 192, 192, 1)',
+                                    borderWidth: 1
+                                }]
+                            },
+                            options: {
+                                scales: {
+                                    y: {
+                                        beginAtZero: true,
+                                        stepSize: 1
+                                    }
+                                }
+                            }
+                        });
+                    });
+                </script>
+            </div>
+
+        </div>
 </x-app-layout>
