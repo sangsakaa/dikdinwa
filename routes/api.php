4af8df5132895776ce8398a2b8b7609e4d14ac15
sangsakaa <zainuri717@gmail.com>
2023-12-24 05:38:21 +0700

    api

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 
 });
-Route::get('/GetMahasiswa', [ApiTestingController::class, 'index'])->name('periode');
+Route::get('/GetMahasiswa', [ApiTestingController::class, 'index'])->name('GetMahasiswa');
